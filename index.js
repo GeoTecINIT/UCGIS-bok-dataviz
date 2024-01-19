2ee06faf713263c48923efb0097c49b5bc39728c
al386134 <al386134@uji.es>
2024-01-19 14:27:24 +0100

    Emergency code editing on rootnode finding

--- index.js
+++ index.js
@@ -132,12 +132,21 @@ export function parseBOKData(bokJSON, v) {
   let rootNode;
 
   for (let i = 0; i < allNodes[v].length; i++) {
-    if (allNodes[v][i].parents.length == 0 && allNodes[v][i].children.length > 0) {
+    console.log(allNodes[v][i].code);
+    //Situational code (for emergencies only)
+    if (allNodes[v][i].code === 'UCGIS'){
+      console.log('Root node now');      
+      rootNode = allNodes[v][i];
+      rootNodeCode = allNodes[v][i].code.toLowerCase();
+      break;
+    }
+      /*
+    //if (allNodes[v][i].parents.length == 0 && allNodes[v][i].children.length > 0) {
       rootNode = allNodes[v][i];
       rootNodeCode = allNodes[v][i].code.toLowerCase();
       console.log("Version " + v + " root node " + i + " code " + rootNodeCode)
-      break;
-    }
+      break;*/
+    //}
   }
 
   return rootNode;
